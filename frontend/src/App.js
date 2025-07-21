8f3c2b09ab5cba8f722c9198cb71a9215431873a
Saketh Bhargava Rallapally <sakethrallapally@gmail.com>
2025-07-22 00:25:36 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import WorkflowBuilder from './components/WorkflowBuilder';
 import './App.css';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
